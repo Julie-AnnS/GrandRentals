5441ce0e312d5d3a3abf20010561e100b8c5c9bc
Nilsu Duran <duranilsu@gmail.com>
2023-02-28 15:28:08 -0500

    added seeds loll

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+Grandparent.destroy_all
 
 10.times do
   User.create!(
-    email: "#{}"
-    language: Faker::Nation.language,
-    phone_number: Faker::PhoneNumber.cell_phone,
-    location: Faker::Space.planet
+    email: Faker::Internet.free_email,
+    password: Faker::Internet.password
   )
 end
 
-10.times do
-  Grandparent.create!(
-    name: Faker::TvShows::BreakingBad.character,
-    age: rand(55...110),
-    abilities: "#{Faker::Hobby.activity}, #{Faker::Hobby.activity}, #{Faker::Hobby.activity}",
-    contradictions: "#{Faker::Hobby.activity}, #{Faker::Hobby.activity}, #{Faker::Hobby.activity}",
-    language: Faker::Nation.language,
-    phone_number: Faker::PhoneNumber.cell_phone,
-    location: Faker::Space.planet
-  )
+User.all.each do |user|
+  rand(1..3).times do
+    gp = Grandparent.new(
+      name: Faker::TvShows::BreakingBad.character,
+      age: rand(55...110),
+      abilities: "#{Faker::Hobby.activity}, #{Faker::Hobby.activity}, #{Faker::Hobby.activity}",
+      contradictions: "#{Faker::Hobby.activity}, #{Faker::Hobby.activity}, #{Faker::Hobby.activity}",
+      language: Faker::Nation.language,
+      phone_number: Faker::PhoneNumber.cell_phone,
+      location: Faker::Space.planet
+    )
+    gp.user = user
+    gp.save!
+  end
+
+  #generate users
 end
