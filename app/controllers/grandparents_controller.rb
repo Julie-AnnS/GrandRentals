938ebfaa62088f259c42a04585a4e578ff3fb01c
Lara Franchi <laradeandrade@protonmail.com>
2023-02-28 14:06:15 -0500

    GrandparentsController&Routes

--- app/controllers/grandparents_controller.rb
+++ app/controllers/grandparents_controller.rb
@@ -1,11 +1,11 @@
 class GrandparentsController < ApplicationController
+  before_action :grandparent, only: [:show, :edit, :update, :destroy]
 
   def index
     @grandparents = Grandparent.all
   end
 
   def show
-    @grandparent = Grandparent.find(params[:id])
   end
 
   def new
@@ -17,9 +17,31 @@ class GrandparentsController < ApplicationController
     if @grandparent.save
       redirect_to grandparent_path(@grandparent)
     else
+      render :new, status: :unprocessable_entity
     end
   end
 
+  # def edit
+  # end
+
+  # def update
+  #   grandparent.update(grandparent_params)
+  #   redirect_to grandparent_path(@grandparent)
+  # end
+
   def destroy
+    @grandparent = Grandparent.find(params[:id])
+    @grandparent.destroy
+    redirect_to grandparents_path, status: :see_other
+  end
+
+  private
+
+  def grandparent_params
+    params.require(:grandparent).permit(:first_name, :last_name, :age, :abilities, :contradictions, :language, :available)
+  end
+
+  def grandparent
+    @grandparent = Grandparent.find(params[:id])
   end
 end
