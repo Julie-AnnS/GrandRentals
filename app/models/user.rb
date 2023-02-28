ddcfefa3eeffa87441ec60ce3d6214303b0fb37b
JulieAnn Selman <julieannselman@gmail.com>
2023-02-28 12:34:05 -0500

    added-validations-to-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :grandparent, dependent: :destroy
   has_many :bookings, dependent: :destroy
+
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :language, presence: true
+  validates :location, presence: true
+  validates :phone_number, presence: true
 end
