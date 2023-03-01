a454dff8307b04d3a934511352d2e8b588b99b0e
Nilsu Duran <duranilsu@gmail.com>
2023-03-01 15:21:56 -0500

    ready for mid demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+Booking.destroy_all
 User.destroy_all
 Grandparent.destroy_all
 
@@ -17,27 +18,28 @@ User.all.each do |user|
       contradictions: "#{Faker::Hobby.activity}, #{Faker::Hobby.activity}, #{Faker::Hobby.activity}",
       language: Faker::Nation.language,
       phone_number: Faker::PhoneNumber.cell_phone,
-      location: Faker::Space.planet
+      location: Faker::Space.planet,
+      daily_rate: Faker::Number.between(from: 30, to: 400)
     )
     gp.user = user
     gp.save!
   end
 
   # 1st scenario - renter
-  renter = User.create!(email: "bob@bob.com", password: "bob123")
-  # 2nd scenario - owner
-  owner = User.create!(email: "ihatemygrandma@gmail.com", password: "grandma123")
+  # renter = User.create!(email: "bob@bob.com", password: "bob123")
+  # # 2nd scenario - owner
+  # owner = User.create!(email: "ihatemygrandma@gmail.com", password: "grandma123")
 
-  owner_grandma = Grandparent.create!(
-    name: "Gertrude", age: 102, abilities: "Knitting, Crocheting, Baking",
-    contradictions: "Running, Climbing down stairs",
-    language: "English", phone_number: 1234567890, location: "Cote-Saint-Luc")
+  # owner_grandma = Grandparent.create!(
+  #   name: "Gertrude", age: 102, abilities: "Knitting, Crocheting, Baking",
+  #   contradictions: "Running, Climbing down stairs",
+  #   language: "English", phone_number: 1234567890, location: "Cote-Saint-Luc", daily_rate: 40)
 
-  owner_grandpa = Grandparent.create!(
-    name: "Benoit", age: 72, abilities: "Cooking, Walking down long distances",
-    contradictions: "Bad language, Smoking",
-    language: "French, English", phone_number: 1726162727, location: "Paris")
+  # owner_grandpa = Grandparent.create!(
+  #   name: "Benoit", age: 72, abilities: "Cooking, Walking down long distances",
+  #   contradictions: "Bad language, Smoking",
+  #   language: "French, English", phone_number: 1726162727, location: "Paris", daily_rate: 40)
 
-  owner_grandpa.user = owner
-  owner_grandma.user = owner
+  # owner_grandpa.user = owner
+  # owner_grandma.user = owner
 end
